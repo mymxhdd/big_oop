df2e0610ad5fe3c7427907f186663eebf333df7c
unknown <>
2016-05-08 14:57:46 +0800

    no general

--- word.h
+++ word.h
@@ -5,16 +5,6 @@
 #include <fstream>
 #include <string>
 using namespace std;
-//单词所需数据，根据需要可以进行修改
-struct General
-{
-    string _word;
-    vecotr<string> _translation;
-    vector<string> _sentence;
-    General(string word, string translation, string * sentence = NULL) : _word(word), 
-    _translation(translation), _sentence(NULL) { }
-    General() {}
-};
 //单词接口类
 class Word
 {
@@ -43,9 +33,11 @@ public:
 
 
 class Rare_word : public Word {
-   General * rareword;
+    string _word;
+    vecotr<string> _translation;
+    vector<string> _sentence;
 public:    
-    Rare_word(string tword, string translation, string * sentence = NULL) { rareword = new General(tword, translation, sentence); }
+    Rare_word(string tword, vector<string> translation, vector<string> sentence): _word(tword), _translation(translation), _sentence(sentence) {}
     void show();
     void translate();
     void learn();
@@ -55,9 +47,11 @@ public:
 
 
 class Familiar_word : public Word {
-    General * familiarword;
+    string _word;
+    vecotr<string> _translation;
+    vector<string> _sentence;
 public:
-    Familiar_word(string tword, string translation, string * sentence = NULL) { familiarword = new General(tword, translation, sentence); }
+    Familiar_word(string tword, vector<string> translation, vector<string> sentence): _word(tword), _translation(translation), _sentence(sentence) {}
     void show();
     void translate();
     void learn();
@@ -66,9 +60,11 @@ public:
 };
 
 class Forget_word : public Word {
-    General * forgetword;
+    string _word;
+    vecotr<string> _translation;
+    vector<string> _sentence;
 public:
-    Forget_word(string tword, string translation, string * sentence = NULL) { forgetword = new General(tword, translation, sentence); }
+    Forget_word(string tword, vector<string> translation, vector<string> sentence): _word(tword), _translation(translation), _sentence(sentence) {}
     void show();
     void translate();
     void learn();
